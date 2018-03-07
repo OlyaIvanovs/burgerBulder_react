98836a8a1c65be31990dea897776233ae10fecc1
Olya Ivanova <olya@ivanovs.info>
2018-03-07 14:03:20 +1300

    Fetch and output orders

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem'
@@ -7,18 +6,8 @@ import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>BurgerBuilder</NavigationItem>
-        <NavigationItem link="/">Checkout</NavigationItem>
-        <NavLink 
-            activeClassName='active'
-            to="/">
-            BurgerBuilder
-        </NavLink>
-        <NavLink 
-            activeClassName='active'
-            to="/checkout">
-            Checkout
-        </NavLink>
+        <NavigationItem link="/" exact>BurgerBuilder</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 );
 
